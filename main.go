063af70bcc3e1bf0d24636d855189732e1d91abb
Dan Berglund <dan@berglund.io>
2024-12-19 21:28:29 +0100

    Re-structure request handling a bit

--- main.go
+++ main.go
@@ -18,25 +18,33 @@ func main() {
 	sessionId := os.Getenv("SESSION_ID")
 	ica := ica.New(sessionId)
 	backend := NewIcaBackend(&ica)
-	handler := caldav.Handler{Backend: backend}
+	handler := withListCache(
+		&caldav.Handler{Backend: backend},
+		&ica,
+	)
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 	slog.Info("Starting")
 
-	log.Fatal(http.ListenAndServe(":5000", WithLogging(&ica, &handler)))
+	log.Fatal(http.ListenAndServe(":5000", withLogging(handler)))
 }
 
-func WithLogging(ica *ica.ICA, h http.Handler) http.Handler {
+func withListCache(h http.Handler, ica *ica.ICA) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// Send in a list-cache, for performance
+		newContext := context.WithValue(r.Context(), "listCache", ListCache{ica: ica})
+		h.ServeHTTP(rw, r.WithContext(newContext))
+	})
+}
+
+func withLogging(h http.Handler) http.Handler {
 	logFn := func(rw http.ResponseWriter, r *http.Request) {
 		lrw := LoggingResponseWriter{rw, ""}
 		body, _ := io.ReadAll(r.Body)
 		r.Body = io.NopCloser(bytes.NewBuffer(body))
 		start := time.Now()
-
-		// Send in a list-cache, for performance
-		newContext := context.WithValue(r.Context(), "listCache", ListCache{ica: ica})
-		h.ServeHTTP(&lrw, r.WithContext(newContext)) // serve the original request
+		h.ServeHTTP(&lrw, r)
 
 		timeElapsed := time.Since(start)
 		// log request details
