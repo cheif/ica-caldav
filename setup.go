51af776308e024a27d441014cbc2721136905f6f
Dan Berglund <dan@berglund.io>
2024-12-19 22:56:41 +0100

    Implement setup frontend with HTMX

--- setup.go
+++ setup.go
@@ -1,14 +1,63 @@
 package main
 
 import (
-    "fmt"
 	"ica-caldav/ica"
 	"net/http"
+	"text/template"
 )
 
-func newServerForSetup(ica *ica.ICA) http.Handler {
-    return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(rw, "Hello, world")
-    })
-}
+func newServerForSetup(icaSession *ica.ICA) http.Handler {
+	mux := http.NewServeMux()
 
+	authenticator := ica.NewBankIDAuthentication(icaSession)
+
+	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// TODO: Check if we actually need setup first
+
+		template, err := template.ParseGlob("templates/*")
+		if err != nil {
+			panic(err)
+		}
+		template.ExecuteTemplate(rw, "index.html", nil)
+	})
+
+	mux.HandleFunc("/start", func(rw http.ResponseWriter, r *http.Request) {
+		template, err := template.ParseGlob("templates/*")
+		if err != nil {
+			panic(err)
+		}
+
+		err = authenticator.Start()
+		if err != nil {
+			template.ExecuteTemplate(rw, "error.html", err)
+			return
+		}
+
+		isFinished, qrCode, err := authenticator.State()
+		if err != nil {
+			template.ExecuteTemplate(rw, "error.html", err)
+		} else if isFinished {
+			template.ExecuteTemplate(rw, "finished.html", nil)
+		} else {
+			template.ExecuteTemplate(rw, "bank-id.html", qrCode)
+		}
+	})
+
+	mux.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
+		template, err := template.ParseGlob("templates/*")
+		if err != nil {
+			panic(err)
+		}
+
+		isFinished, qrCode, err := authenticator.State()
+		if err != nil {
+			template.ExecuteTemplate(rw, "error.html", err)
+		} else if isFinished {
+			template.ExecuteTemplate(rw, "finished.html", nil)
+		} else {
+			template.ExecuteTemplate(rw, "bank-id.html", qrCode)
+		}
+	})
+
+	return mux
+}
