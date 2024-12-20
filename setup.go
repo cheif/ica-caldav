b1d317f81702de430481a88665b0b2164c608593
Dan Berglund <dan@berglund.io>
2024-12-20 08:52:04 +0100

    Restructure setup.go

--- setup.go
+++ setup.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"ica-caldav/ica"
+	"io"
 	"net/http"
 	"text/template"
 )
@@ -13,51 +14,45 @@ func newServerForSetup(icaSession *ica.ICA) http.Handler {
 
 	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		// TODO: Check if we actually need setup first
-
-		template, err := template.ParseGlob("templates/*")
-		if err != nil {
-			panic(err)
-		}
-		template.ExecuteTemplate(rw, "index.html", nil)
+		executeTemplate(rw, "index.html", nil)
 	})
 
 	mux.HandleFunc("/start", func(rw http.ResponseWriter, r *http.Request) {
-		template, err := template.ParseGlob("templates/*")
+		err := authenticator.Start()
 		if err != nil {
-			panic(err)
-		}
-
-		err = authenticator.Start()
-		if err != nil {
-			template.ExecuteTemplate(rw, "error.html", err)
+			executeTemplate(rw, "error.html", err)
 			return
 		}
 
 		isFinished, qrCode, err := authenticator.State()
 		if err != nil {
-			template.ExecuteTemplate(rw, "error.html", err)
+			executeTemplate(rw, "error.html", err)
 		} else if isFinished {
-			template.ExecuteTemplate(rw, "finished.html", nil)
+			executeTemplate(rw, "finished.html", nil)
 		} else {
-			template.ExecuteTemplate(rw, "bank-id.html", qrCode)
+			executeTemplate(rw, "bank-id.html", qrCode)
 		}
 	})
 
 	mux.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
-		template, err := template.ParseGlob("templates/*")
-		if err != nil {
-			panic(err)
-		}
-
 		isFinished, qrCode, err := authenticator.State()
 		if err != nil {
-			template.ExecuteTemplate(rw, "error.html", err)
+			executeTemplate(rw, "error.html", err)
 		} else if isFinished {
-			template.ExecuteTemplate(rw, "finished.html", nil)
+			executeTemplate(rw, "finished.html", nil)
 		} else {
-			template.ExecuteTemplate(rw, "bank-id.html", qrCode)
+			executeTemplate(rw, "bank-id.html", qrCode)
 		}
 	})
 
 	return mux
+}
+
+// It's probably quicker to just fetch templates once, but this makes it support live-reload.
+func executeTemplate(rw io.Writer, name string, val any) {
+	template, err := template.ParseGlob("templates/*")
+	if err != nil {
+		panic(err)
+	}
+	template.ExecuteTemplate(rw, name, val)
 }
